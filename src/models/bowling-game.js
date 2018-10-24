15cfc4f2ce24593f991a555c13f17500db720c2b
sushmamurthy <sushma.murthy@blockbid.io>
2018-10-24 17:16:02 +1100

    Implemented bowling score logic and test scenarios

--- src/models/bowling-game.js
+++ src/models/bowling-game.js
@@ -1,1 +1,55 @@
-// TODO Create a bowling class with rolls and score calculator fn
+class BowlingGame {
+    constructor(){
+        this.rolls = [];
+    }
+
+    _isSpare(rollIndex) {
+        return (this.rolls[rollIndex] + this.rolls[rollIndex + 1] === 10);
+    }
+
+    // TODO handle strike case in last frame
+    _isStrike(rollIndex) {
+        return (this.rolls[rollIndex] === 10);
+    }
+
+    _getFrameScore(rollIndex, noOfRollsToSum){
+        let score = 0;
+        let i = 0;
+
+        // noOfRollsToSum handles open frame, spare and strike calculation
+        while(i < noOfRollsToSum){
+            score += this.rolls[rollIndex] !== undefined && this.rolls[rollIndex];
+            rollIndex++;
+            i++;
+        }
+        return score;
+    }
+
+    roll(pins) {
+        this.rolls.push(pins)
+    }
+
+    score(){
+        let totalScore = 0;
+        let rollIndex = 0;
+
+        for(let frame = 0; frame < 10; frame++){
+            if (rollIndex < this.rolls.length){
+                if (this._isStrike(rollIndex)){
+                    totalScore += this._getFrameScore(rollIndex, 3);
+                    rollIndex++;
+                } else if (this._isSpare(rollIndex)){
+                    totalScore += this._getFrameScore(rollIndex, 3);
+                    rollIndex += 2;
+                } else {
+                    totalScore += this._getFrameScore(rollIndex, 2);
+                    rollIndex += 2;
+                }
+            }
+        }
+
+        return totalScore;
+    }
+}
+
+module.exports = BowlingGame;
--- test/models/bowling-game.test.js
+++ test/models/bowling-game.test.js
@@ -1,1 +1,62 @@
-// TODO Add tests for bowling game, cover all the bowling test scenarios
+const BowlingGame = require('../../src/models/bowling-game');
+
+describe('Bowling Game', () => {
+    let player;
+
+    beforeEach(() => {
+        player = new BowlingGame();
+    });
+
+    it('rolls the pin', () => {
+        player.roll(1);
+        expect(player.score()).toBe(1);
+    });
+
+    it('rolls ball to gutter', () => {
+        player.roll(0);
+        expect(player.score()).toBe(0);
+    });
+
+    it('multiple rolls', () => {
+        player.roll(5);
+        player.roll(3);
+        player.roll(8);
+        expect(player.score()).toBe(16);
+    });
+
+    it('multiple rolls with a spare', () => {
+        player.roll(5);
+        player.roll(5);
+        player.roll(3);
+        expect(player.score()).toBe(16);
+    });
+
+    it('multiple rolls with a strike', () => {
+        player.roll(10);
+        player.roll(5);
+        player.roll(3);
+        expect(player.score()).toBe(26);
+    });
+
+    it('multiple rolls with a spare and a strike', () => {
+        player.roll(5);
+        player.roll(5);
+        player.roll(10);
+        player.roll(3);
+        player.roll(4);
+        expect(player.score()).toBe(44);
+    });
+
+    it('full game', () => {
+        // Add rolls for all 10 frames
+    });
+
+    it('full game with a strike in the last frame', () => {
+        // Add rolls for all 10 frames
+    });
+
+    it('full game with all strikes', () => {
+        // Add rolls for all 10 frames
+    });
+
+});
